898c976d7ae06ac671f0e57a1f38226d1696fe97
yomemll <yomememll@gmail.com>
2026-03-06 20:01:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -82,6 +82,7 @@ const drawingBoard = document.getElementById('canvas');
 const ctx = drawingBoard.getContext('2d');
 const playerTypeChange = document.getElementById('player');
 const gameStart = document.getElementById('start');
+const restrat = document.getElementById('restart');
 
 let playerType=0;
 
@@ -89,6 +90,23 @@ let x = 10;
 let y = 10;
 let xx = 30;
 let yy = 30;
+
+let shootingX=x;
+let shootingY=y;
+let shootingXX=xx;
+let shootingYY=yy;
+
+let shootingMovement=10;
+let shootingMovement2=10;
+
+let playerAim=0;
+let playerAim2=0;/*
+null=0
+Arrow up=1
+Arrow down=2
+Arrow left=3
+Arrow down=4
+*/
 
 const moveAmount = 5;
 
@@ -139,22 +157,16 @@ document.addEventListener('keydown', async (e) => {
     if (keys.hasOwnProperty(e.key)) {
         keys[e.key] = true;
     }
-    await updateLocation(x,y,xx,yy);
-    if(playerType==1){//player type redbit
-        const tempLocation = await getPlayerLocation();
-        x=tempLocation[0];
-        y=tempLocation[1];
-    } else if(playerType==0){//player type bluebit
-        const tempLocation = await getPlayerLocation();
-        xx=tempLocation[0];
-        yy=tempLocation[1];
-    }
 });
 
 document.addEventListener('keyup', (e) => {
     if (keys.hasOwnProperty(e.key)) {
         keys[e.key] = false;
     }
+    shootingX=x;
+    shootingY=y;
+    shootingXX=xx;
+    shootingYY=yy;
 });
 
 playerTypeChange.addEventListener('click', () => {
@@ -169,4 +181,99 @@ playerTypeChange.addEventListener('click', () => {
 
 gameStart.addEventListener('click', () => {
     sendLocationToServer(x,y,xx,yy);
+});
+
+async function liveLocateUpdate(){
+    await updateLocation(x,y,xx,yy);
+    if(playerType==1){//player type redbit
+        const tempLocation = await getPlayerLocation();
+        x=tempLocation[0];
+        y=tempLocation[1];
+    } else if(playerType==0){//player type bluebit
+        const tempLocation = await getPlayerLocation();
+        xx=tempLocation[0];
+        yy=tempLocation[1];
+    }
+}
+
+setInterval(liveLocateUpdate,16);
+
+restrat.addEventListener('click', async()=>{
+    const docRef = doc(db, "players", "player1");
+    const docRef2 = doc(db, "players", "player2");
+    await updateDoc(docRef, {
+        X: 10,
+        Y: 10
+    });
+    await updateDoc(docRef2, {
+        XX: 30,
+        YY: 30
+    });
+});
+
+function shootingPlayer1() {
+    if(playerAim==1) {
+        drawShooting();
+        shootingY=shootingY-shootingMovement;
+    } else if(playerAim==2) {
+        drawShooting();
+        shootingY=shootingY+shootingMovement;
+    } else if(playerAim==3) {
+        drawShooting();
+        shootingX=shootingX-shootingMovement;
+    } else if(playerAim==4) {
+        drawShooting();
+        shootingX=shootingX+shootingMovement;
+    }
+}
+function shootingPlayer2() {
+    if(playerAim2==1) {
+        drawShooting();
+        shootingYY=shootingYY-shootingMovement2;
+    } else if(playerAim2==2) {
+        drawShooting();
+        shootingYY=shootingYY+shootingMovement2;
+    } else if(playerAim2==3) {
+        drawShooting();
+        shootingXX=shootingXX-shootingMovement2;
+    } else if(playerAim2==4) {
+        drawShooting();
+        shootingXX=shootingXX+shootingMovement2;
+    }
+}
+function drawShooting() {
+    if(playerType==0){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(shootingX, shootingY, 10, 10);
+    }else if(playerType==1){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(shootingXX, shootingYY, 10, 10);
+    }
+}
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+        if(playerType==0){playerAim=1}
+        if(playerType==1){playerAim2=1}
+    }
+    if (e.key === 'ArrowDown') {
+        if(playerType==0){playerAim=2}
+        if(playerType==1){playerAim2=2}
+    }
+    if (e.key === 'ArrowLeft') {
+        if(playerType==0){playerAim=3}
+        if(playerType==1){playerAim2=3}
+    }
+    if (e.key === 'ArrowRight') {
+        if(playerType==0){playerAim=4}
+        if(playerType==1){playerAim2=4}
+    }
+});
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'w') {
+        if(playerType==0){
+            shootingPlayer1();
+        }else if (playerType==1){
+            shootingPlayer2();
+        }
+    }
 });
